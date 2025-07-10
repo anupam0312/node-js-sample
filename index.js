7f60e6e5d89399cc718b42ce9524b0d69796020e
anupam0312 <anupammorvanchikar1@gmail.com>
2025-07-10 12:32:30 +0530

    Update index.js
    Signed-off-by: anupam0312 <anupammorvanchikar1@gmail.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Anupam.!')
+  response.send('Hello Anupam. How are you!')
 })
 
 app.listen(app.get('port'), function() {
